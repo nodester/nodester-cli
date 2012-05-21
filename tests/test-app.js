7441fcedb47a4340830bc1514182a8f2337d5275
Alejandro Morales <vamg008@gmail.com>
2012-05-20 22:17:34 -0600

    [refactor] 25 Tests are passing now

--- tests/test-app.js
+++ tests/test-app.js
@@ -1,12 +1,49 @@
 #!/usr/bin/env node
 
 process.nodester = {
-    brand: 'nodester',
-    apihost: 'api.nodester.com'
+  brand: 'nodester',
+  apihost: 'api.nodester.com',
+  env: 'test'
 }
 
-var cli = require('nodester-cli'),
-    command = process.argv[0],
-    cmds = cli.commands;
-    
-cli.run(cmds, command);
+var cli = require('../lib/commands')
+  , cmds = cli.commands
+  ;
+
+var assert = require('assert');
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+describe('General API test', function(){
+  describe('should read all the commands', function (){
+    for (var key in cmds){
+      it('should run ' + key,function(){
+        cli.run(cmds,key)
+      })
+    }
+  });
+  describe('General usage', function(){
+    for (var key in cmds.user){
+      it('should run ' + key,function(){
+        cli.run(cmds,'user',key)
+      })
+    }
+  });
+  describe('App endpoint', function(){
+    it('should return an error, invalid app', function(){
+        cli.run(cmds,'app','infods','a')
+    })
+  });
+  describe('Errors', function(){
+    it('should return and error, invalid command', function(){
+      // assert.throws doesn't work quite well
+      try {
+        cli.run(cmds,'ras')
+      } catch(ex){
+        assert.equal(ex && true, true)
+      }
+    })
+  })
+})
