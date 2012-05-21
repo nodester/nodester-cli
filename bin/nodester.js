8114d315f9eb97f09dbb6d44cd81c4e202037fce
Alejandro Morales <vamg008@gmail.com>
2012-05-20 22:16:17 -0600

    [refactor] Reference to local lib and env vars handler

--- bin/nodester.js
+++ bin/nodester.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
+
 process.nodester = {
-    brand: 'nodester',
-    apihost: 'api.nodester.com'
+  brand: 'nodester',
+  apihost: 'api.nodester.com',
+  env : process.env.NODE_ENV || 'production'
 }
 
-var cli = require('nodester-cli'),
+var cli = require('../lib/commands'),
     command = process.argv[0],
     cmds = cli.commands;
-    
+
 cli.run(cmds, command);
